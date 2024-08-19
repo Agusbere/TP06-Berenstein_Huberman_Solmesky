0fbc38ff8a1f6c9598b63db34e8caf2afac0bd3a
BRIAN GABRIEL HUBERMAN <48179712@ort>
2024-08-19 14:28:46 -0300

    Comenzado el footer

--- obj/Debug/net8.0/TP06-Berenstein_Huberman_Solmesky.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP06-Berenstein_Huberman_Solmesky.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP06-Berenstein_Huberman_Solmesky")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58d9eed7e4ea1fd73ec1af88dd589d8512bf759")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbdbce98391ff801172660f27a0d1e52a2baafd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP06-Berenstein_Huberman_Solmesky")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP06-Berenstein_Huberman_Solmesky")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
